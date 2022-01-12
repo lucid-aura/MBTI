3becc5919392e6abcfaf47f512ea77d441968564
charmPHJ <92033764+charmPHJ@users.noreply.github.com>
2022-01-12 15:21:01 +0900

    12
    12

--- src/main/java/secure/mbti/a/dao/BoardDao.java
+++ src/main/java/secure/mbti/a/dao/BoardDao.java
@@ -2,16 +2,20 @@ package secure.mbti.a.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
 import secure.mbti.a.dto.BoardDto;
 import secure.mbti.a.dto.BoardParam;
 
 public interface BoardDao {
+	
 	List<BoardDto> board_list(BoardParam param);
 	
 	int board_write(BoardDto dto);
 	
 	BoardDto getBoard(int seq);
-	
+	//  crud  r = 행을 반환
+	// cud = int 로 반환되고 void로 반환 안되는경우도있다 마
 	
 	
 }
--- src/main/java/secure/mbti/a/dao/BoardDaoImpl.java
+++ src/main/java/secure/mbti/a/dao/BoardDaoImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import secure.mbti.a.dto.BoardDto;
 import secure.mbti.a.dto.BoardParam;
 
+@Repository
 public class BoardDaoImpl implements BoardDao{
 	@Autowired
 	SqlSession session;		// mybatis 사용하기 위해
@@ -24,5 +26,10 @@ public class BoardDaoImpl implements BoardDao{
 		
 		int count = session.insert(ns + "board_write", dto);
 		return count;
+	}
+
+	@Override
+	public BoardDto getBoard(int seq) {
+		return session.selectOne(ns + "getBoard", seq);
 	}
 }
