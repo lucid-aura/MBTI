4f91ed6683dcb6ca27fed56f8c5cad9519a458cc
lucid-aura <lucidaura@daum.net>
2022-01-12 15:11:27 +0900

    Comment DB, Dto, Dao, Service 작성
    Comment DB, Dto, Dao, Service 작성에 따른 컨트롤러 연결, jsp 변경

---
+++ src/main/java/secure/mbti/a/dao/CommentDaoImpl.java
@@ -0,0 +1,51 @@
+package secure.mbti.a.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import secure.mbti.a.dto.CommentDto;
+
+@Repository
+public class CommentDaoImpl implements CommentDao {
+
+	@Autowired
+	SqlSession session;
+
+	String ns = "Comment.";
+	
+	@Override
+	public List<CommentDto> comment_list(int boardseq) {
+		System.out.println(boardseq);
+		return session.selectList(ns+"commentlist", boardseq);
+	}
+
+	@Override
+	public int comment_getcount(int boardseq) {
+		return session.selectOne(ns + "commentcount", boardseq);
+	}
+
+	@Override
+	public void comment_countUp(int boardseq) {
+		session.update(ns + "commentcountup", boardseq);
+	}
+	
+	@Override
+	public void comment_write(CommentDto dto) {
+		session.insert(ns + "commentwrite", dto);
+	}
+
+	@Override
+	public void comment_update(CommentDto dto) {
+		session.update(ns + "comment_update", dto);
+	}
+
+	@Override
+	public void comment_delete(int commentseq) {
+		session.delete(ns + "commentdelete", commentseq);
+	}
+	
+}
+
